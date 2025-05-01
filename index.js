6f678577d862dbea9c544d5eca6971f2b1fe29cf
sandeep0302 <gsandeepgoud0302@gmail.com>
2025-05-01 21:09:15 +0530

    Working API with Winston

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger= require('./logger');
+
+const app = express();
+app.use(express.json());
+
+app.post('/v1/sample/intro',(req,res) => {
+    logger.info('--- Request received at /v1/sample/intro ---');
+    logger.info(`Headers: ${JSON.stringify(req.headers,null,2)}`);
+
+    res.json({
+        name:"sandeep",
+        age: 29,
+        profession: " Software Developer",
+        city: "bangalore",
+        country: "india",
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+});
